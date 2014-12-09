fe5ef8aba1510c0085d30d0b4b9b6f2ac154f53a
robyabbts <chregi.sommer@gmail.com>
2014-12-09 18:57:08 +0100

    add modul

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -62,7 +62,7 @@ return array(
         ),
     ),
     'translator' => array(
-        'locale' => 'en_US',
+        'locale' => 'de_DE',
         'translation_file_patterns' => array(
             array(
                 'type'     => 'gettext',
