389076070991540d8d28f00d1658836f1ef9ac9c
robyabbts <chregi.sommer@gmail.com>
2014-12-18 00:20:00 +0100

    setup basic Double-opt-in

---
+++ module/User/src/User/Factory/Service/DoubleOptInServiceFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DoubleOptInServiceFactory
+ *
+ * @author win7
+ */
+class DoubleOptInServiceFactory {
+    //put your code here
+}
