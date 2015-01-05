92afb0d446450c3ac6052ba3921fbd88068e725c
michaelrohr <michael.rohr@bluewin.ch>
2015-01-05 21:57:39 +0100

    ad register form

--- module/User/src/User/Form/Zfcuser/Register.php
+++ module/User/src/User/Form/Zfcuser/Register.php
@@ -5,9 +5,9 @@ namespace User\Form\Zfcuser;
 use Zend\Form\Element\Captcha as Captcha;
 use ZfcUser\Options\RegistrationOptionsInterface;
 
-class Register extends \ZfcUser\Form\Base
-{
-    protected $captchaElement= null;
+class Register extends \ZfcUser\Form\Base {
+
+    protected $captchaElement = null;
 
     /**
      * @var RegistrationOptionsInterface
@@ -18,28 +18,49 @@ class Register extends \ZfcUser\Form\Base
      * @param string|null $name
      * @param RegistrationOptionsInterface $options
      */
-    public function __construct($name, RegistrationOptionsInterface $options)
-    {
+    public function __construct($name, RegistrationOptionsInterface $options) {
         $this->setRegistrationOptions($options);
         parent::__construct($name);
 
         $this->remove('userId');
+
+
         if (!$this->getRegistrationOptions()->getEnableUsername()) {
             $this->remove('username');
         }
-        if (!$this->getRegistrationOptions()->getEnableDisplayName()) {
-            $this->remove('display_name');
-        }
+
         if ($this->getRegistrationOptions()->getUseRegistrationFormCaptcha() && $this->captchaElement) {
-            $this->add($this->captchaElement, array('name'=>'captcha'));
+            $this->add($this->captchaElement, array('name' => 'captcha'));
         }
+
+        $this->add(array('name' => 'street', 'options' => array('label' => 'Strasse',),
+            'attributes' => array('type' => 'text',), 'required',
+                )
+        );
+
+        $this->add(
+                array('name' => 'plz', 'options' => array('label' => 'Postleitzahl',),
+                    'attributes' => array('type' => 'text',), 'required',
+                )
+        );
+        $this->add(
+                array('name' => 'village', 'options' => array('label' => 'Ort',),
+                    'attributes' => array('type' => 'text',), 'required',
+                )
+        );
+
+        $this->add(array('name' => 'phone', 'options' => array('label' => 'Telefon',),
+            'attributes' => array('type' => 'tel', 'required'),
+                )
+        );
+
+
         $this->get('submit')->setLabel('Register');
         $this->getEventManager()->trigger('init', $this);
     }
 
-    public function setCaptchaElement(Captcha $captchaElement)
-    {
-        $this->captchaElement= $captchaElement;
+    public function setCaptchaElement(Captcha $captchaElement) {
+        $this->captchaElement = $captchaElement;
     }
 
     /**
@@ -48,8 +69,7 @@ class Register extends \ZfcUser\Form\Base
      * @param RegistrationOptionsInterface $registrationOptions
      * @return Register
      */
-    public function setRegistrationOptions(RegistrationOptionsInterface $registrationOptions)
-    {
+    public function setRegistrationOptions(RegistrationOptionsInterface $registrationOptions) {
         $this->registrationOptions = $registrationOptions;
         return $this;
     }
@@ -59,8 +79,8 @@ class Register extends \ZfcUser\Form\Base
      *
      * @return RegistrationOptionsInterface
      */
-    public function getRegistrationOptions()
-    {
+    public function getRegistrationOptions() {
         return $this->registrationOptions;
     }
+
 }
