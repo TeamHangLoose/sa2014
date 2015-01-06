319f632e41987667b77e229d93a74706712591ed
robyabbts <chregi.sommer@gmail.com>
2015-01-06 17:27:44 +0100

    00

--- module/User/src/User/Form/htProfileImage/ProfileImageForm.php
+++ module/User/src/User/Form/htProfileImage/ProfileImageForm.php
@@ -14,6 +14,7 @@ class ProfileImageForm extends ProvidesEventsForm {
         $this->add([
             'name' => 'image',
             'type' => 'File',
+            'class'=>'img-circle',
         ]);
 
         $this->add([
