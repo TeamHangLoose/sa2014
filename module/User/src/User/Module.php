fc2d0d192ce986f1c2c1b49975873b63faa208e4
robyabbts <chregi.sommer@gmail.com>
2015-01-08 13:35:16 +0100

    dd

--- module/User/src/User/Module.php
+++ module/User/src/User/Module.php
@@ -63,7 +63,12 @@ class Module {
 
     public function getViewHelperConfig() {
         return array(
+            'aliases' => array(
+               // 'htProfileImage' => 'User\View\Helper\ProfileImage'
+            ),
+      
             'factories' => array(
+               // 'User\View\Helper\ProfileImage' => 'HtProfileImage\View\Helper\Factory\ProfileImageFactory',
                 'AccountDisplay' => function ($sm) {
                     $locator = $sm->getServiceLocator();
                     $viewHelper = new \User\View\Helper\AccountDisplay;
