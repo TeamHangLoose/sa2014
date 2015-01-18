59c44ff0a9419b51c9105cc87fc2a708761975f2
robyabbts <chregi.sommer@gmail.com>
2015-01-18 22:50:22 +0100

    Revert "Revert "c""
    This reverts commit 85979f5c77c00c630720ce6283a7288af0c94fdb.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -12,7 +12,7 @@ return array(
             'home' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
                 'options' => array(
-                    'route' => '/home',
+                    'route' => '/',
                     'defaults' => array(
                         'controller' => 'Application\Controller\Index',
                         'action' => 'index',
