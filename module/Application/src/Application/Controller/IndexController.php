c322e773fc1ded6d40a99647428962a4e732a691
robyabbts <chregi.sommer@gmail.com>
2015-02-01 21:39:03 +0100

    finisch

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,21 +1,23 @@
 <?php
+
 namespace Application\Controller;
-/* 
+
+/*
  * @license http://framework.zend.com/license/new-bsd New BSD License
  * @author  abbts2015 B14.if4.1 G.3
  */
+
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        return new ViewModel();
-  
-    }
-          public function aboutAction()
-    {
+class IndexController extends AbstractActionController {
+
+    public function indexAction() {
         return new ViewModel();
     }
+
+    public function aboutAction() {
+        return new ViewModel();
+    }
+
 }
