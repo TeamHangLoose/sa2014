5bff9a31e99d3638f7aeb6d00f8afca84f2e5cf1
michaelrohr <michael.rohr@bluewin.ch>
2015-02-05 10:03:23 +0100

    fix german

--- config/application.config.php
+++ config/application.config.php
@@ -17,6 +17,7 @@ return array(
         'HtImgModule',
         'Application',
         'User',
+        'WebsafeZfModZfcUserI18nDeDe',
     ),
     // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
